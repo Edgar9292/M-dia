117db99e3c2a7e49d6d737075d1ca27c637f6d2d
Edgar9292 <92225390+Edgar9292@users.noreply.github.com>
2022-01-15 17:20:41 -0300

    Add files via upload

---
+++ Média.cpp
@@ -0,0 +1,21 @@
+
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+#include <iostream>
+int main (){
+float media;
+cout <<"digite a nota1:";
+cin >>nota1;
+cout <<"digite nota2:";
+cin >>nota2;
+media = (n1+n2)/2;
+if (media > == 60){
+	cout<<"aprovado:";
+	cout<<""media:"" << media;
+}
+if(media < 60){
+	cout <<"reprovado";
+	cout<<""media:""<< media;
+}
+}
